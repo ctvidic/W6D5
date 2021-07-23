c43e983613fd13aa14ae0d4a6f13fc2b4ef4ca42
Quinn Mullaney <skiingqm@gmail.com>
2021-07-23 14:38:37 -0400

    Starting routes and controllers for cats

--- Cats_website/app/models/cat.rb
+++ Cats_website/app/models/cat.rb
@@ -2,6 +2,15 @@ require 'action_view'
 class Cat < ApplicationRecord
     include 'date'
     ActionView::Helpers::DateHelper
+
+    COLORS = ['brown', 'orange', 'black', 'grey']
+    validations :color, inclusion: COLORS 
+    SEX = ["M", "F"]
+    validations :sex, inclusion: SEX 
+    validations :birth_date, :color, :name, :sex, :description, presence: true
+
+    
+
     def age
         currentTime = Date.today
         ((currentTime  - Cat.birth_date).to_i)/365
